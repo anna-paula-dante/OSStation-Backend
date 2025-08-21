2c3d40f22cbd1cc5fab6c752679153faf08302fd
Ana Paula <anajornalistasbc@gmail.com>
2025-08-21 13:53:42 -0300

    Fix: use use cors to frontend works correctly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const apiRoutes = require('./api/routes');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 
 app.use(express.json())
 app.use('/api', apiRoutes);
-
 app.get('/', (request, response) => {
     response.send(`Hello World`)
 })
